fe6c21c94b91a73f5e9f77b5730ecc15bae82ed9
Zümra <zumrakb8@gmail.com>
2024-12-14 00:58:51 +0300

    #2: updates

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,12 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Enable Static Export
-  output: "export", // Add this line for static export
 
+  /*  experimental: {
+    serverActions: {
+      bodySizeLimit: "100MB",
+    },
+  }, */
   images: {
     remotePatterns: [
       {
