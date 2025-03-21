d7f351f6db0f326ea313b9a5f1cd72f41bf49413
Zümra <zumrakb8@gmail.com>
2025-03-22 01:57:03 +0300

    feature: Continue as guest button is added to auth layout.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { AuthHeader } from "@/components";
+import { AuthHeader, Button } from "@/components";
 import React from "react";
 import { usePathname } from "next/navigation";
 
@@ -16,52 +16,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         }
       />
       {children}
-      {/* <div className="flex flex-col gap-4">
-        <Button
-          name={
-            componentType === constants.pageType.signup ? "Signup" : "Login"
-          }
-          style="secondary"
-          className="w-full py-3"
-        />
-        <div className="flex gap-1 items-center justify-center opacity-50 text-xs">
-          <Separator />
-          <span>or</span>
-          <Separator />
-        </div>
-        <Button
-          name={
-            componentType === constants.pageType.signup
-              ? "Signup with Google"
-              : "Login with Google"
-          }
-          style="general"
-          className="w-full py-3 border border-white/40 text-smoothWhite hover:bg-white/40"
-          icon={<FcGoogle size={20} />}
-        />
-        <div className=" ">
-          {componentType === constants.pageType.signup ? (
-            <span className="flex items-center justify-center font-semibold gap-1 text-xs">
-              Already have an account?{" "}
-              <Anchor
-                style={"general"}
-                name="Login"
-                url={constants.pageType.login}
-                className=" text-purple-500 p-0 m-0 text-sm"
-              />
-            </span>
-          ) : (
-            <span className="flex items-center justify-center font-semibold text-xs gap-1">
-              New on movmash?{" "}
-              <Anchor
-                name="Signup"
-                url={constants.pageType.signup}
-                className=" m-0 p-0 text-purple-500"
-              />
-            </span>
-          )}
-        </div>
-      </div> */}
+      <Button
+        onClick={() => console.log("continue as guest button is clicked.")}
+        name="Continue as guest."
+        className="text-xs p-0 m-0 w-fit mx-auto font-medium underline underline-offset-2 hover:text-white"
+      />
     </div>
   );
 };
