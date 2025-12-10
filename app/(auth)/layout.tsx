8e7ef0ec1066b553e4a69310094020335857355a
Ankur Kunal <ankur.kunal@ibm.com>
2025-12-10 18:43:51 +0530

    Update with proper import for logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -12,14 +12,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative w-full h-full bg-[#18181b] flex flex-col items-center justify-center overflow-hidden min-h-screen px-4 py-8">
       <div className="w-full max-w-md flex flex-col items-center gap-8">
-        {/* Logo and Title
-        <header className="flex flex-col items-center justify-center gap-4">
-          <Image src="/assets/logo.svg" alt="logo" width={70} height={70} />
-          <h3 className="text-2xl font-extrabold text-white text-center font-parkinsans tracking-tight -mt-2">Movmash</h3>
-          <h1 className="text-3xl text-center font-bold text-white">{title}</h1>
-        </header> */}
-
-        {/* Auth content */}
         <div className="w-full">
           {children}
         </div>
