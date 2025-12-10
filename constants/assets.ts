38df1c8bbe4e1b1570b6ebf7d57cad1ef0a58842
Ankur Kunal <ankur.kunal@ibm.com>
2025-12-10 18:41:18 +0530

    Update with proper import for logo

--- constants/assets.ts
+++ constants/assets.ts
@@ -1,5 +1,6 @@
-import logo from '../public/assets/logo.svg'
-import visualizerLogo from "../public/assets/logo-visualizer1.svg";
+// Use string paths for static assets in Next.js public directory
+const logo = '/assets/logo.svg';
+const visualizerLogo = '/assets/logo-visualizer1.svg';
 const defaultAvatar =
   "https://png.pngtree.com/png-vector/20231019/ourmid/pngtree-user-profile-avatar-png-image_10211471.png";
 export { logo, defaultAvatar, visualizerLogo };
