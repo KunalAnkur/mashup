a9d10f8a0ba26b3a17cc56bc4da2c004118e7613
Ankur Kunal <ankur.kunal@ibm.com>
2025-12-12 02:25:22 +0530

    Update the instance ip

--- hooks/useStream.tsx
+++ hooks/useStream.tsx
@@ -406,8 +406,29 @@ export const useStream = ({
 
             if (isHost) {
                 if (!joinResponse.sendTransportOptions) throw new Error("No sendTransportOptions");
+                console.log("[STREAM] Creating send transport", joinResponse.sendTransportOptions);
+                const iceServers = [
+                    {
+                        urls: [
+                            "stun:gateway.movmash.com:3478",
 
-                const transport = device.createSendTransport(joinResponse.sendTransportOptions);
+                            // TURN (UDP)
+                            "turn:gateway.movmash.com:3478?transport=udp",
+
+                            // TURN (TCP)
+                            "turn:gateway.movmash.com:3478?transport=tcp",
+
+                            // TURN over TLS (secure)
+                            "turns:gateway.movmash.com:5349?transport=tcp"
+                        ],
+                        username: "turn_user",
+                        credential: "0a6c9d2b2948690"
+                    }
+                ]
+                const transport = device.createSendTransport({
+                    ...joinResponse.sendTransportOptions,
+                    iceServers,
+                });
                 createConnectHandler(transport, roomId);
 
                 transport.on("produce", async ({ kind, rtpParameters }, callback, errback) => {
@@ -432,8 +453,28 @@ export const useStream = ({
                 if (stream) await createProducers(transport, stream, roomId);
             } else {
                 if (!joinResponse.recvTransportOptions) throw new Error("No recvTransportOptions");
+                const iceServers = [
+                    {
+                        urls: [
+                            "stun:gateway.movmash.com:3478",
 
-                const transport = device.createRecvTransport(joinResponse.recvTransportOptions);
+                            // TURN (UDP)
+                            "turn:gateway.movmash.com:3478?transport=udp",
+
+                            // TURN (TCP)
+                            "turn:gateway.movmash.com:3478?transport=tcp",
+
+                            // TURN over TLS (secure)
+                            "turns:gateway.movmash.com:5349?transport=tcp"
+                        ],
+                        username: "turn_user",
+                        credential: "0a6c9d2b2948690"
+                    }
+                ]
+                const transport = device.createRecvTransport({
+                    ...joinResponse.recvTransportOptions,
+                    iceServers,
+                });
                 createConnectHandler(transport, roomId);
                 consumerTransportRef.current = transport;
 
