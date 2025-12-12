25af3d12186d7322bf629a94da0a0badf6ceaedb
Ankur Kunal <ankur.kunal@ibm.com>
2025-12-12 20:51:20 +0530

    Update landing page

--- constants/turnConstant.ts
+++ constants/turnConstant.ts
@@ -2,15 +2,12 @@ const turnConfig = {
   iceServers: [
     {
       urls: [
-        "turn:turn.movmash.com:3478?transport=udp",
-        "turn:turn.movmash.com:3478?transport=tcp",
-        "turns:turn.movmash.com:5349?transport=tcp",
+        "turn:turn.movmash.com:3478",
       ],
       username: "movmash",
       credential: "0a6c9d2b2948690",
     },
   ],
-  iceTransportPolicy: "relay",
 };
 
 export default turnConfig;
--- hooks/useStream.tsx
+++ hooks/useStream.tsx
@@ -369,7 +369,6 @@ export const useStream = ({
             const newTransport = newDevice.createRecvTransport({
                 ...response.recvTransportOptions,
                 iceServers: turnConfig.iceServers,
-                iceTransportPolicy: turnConfig.iceTransportPolicy,
             });
             createConnectHandler(newTransport, roomId);
             consumerTransportRef.current = newTransport;
@@ -415,7 +414,6 @@ export const useStream = ({
                 const transport = device.createSendTransport({
                     ...joinResponse.sendTransportOptions,
                     iceServers: turnConfig.iceServers,
-                    iceTransportPolicy: turnConfig.iceTransportPolicy,
                 });
                 createConnectHandler(transport, roomId);
 
@@ -444,7 +442,6 @@ export const useStream = ({
                 const transport = device.createRecvTransport({
                     ...joinResponse.recvTransportOptions,
                     iceServers: turnConfig.iceServers,
-                    iceTransportPolicy: turnConfig.iceTransportPolicy,
                 });
                 createConnectHandler(transport, roomId);
                 consumerTransportRef.current = transport;
