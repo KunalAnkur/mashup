11ceceaa9250daff817e8b42821eb2b631db75c0
Ankur Kunal <ankur.kunal@ibm.com>
2025-12-13 14:24:17 +0530

    Update landing page

--- constants/turnConstant.ts
+++ constants/turnConstant.ts
@@ -1,9 +1,13 @@
 const turnConfig = {
   iceServers: [
     {
-      urls: ["turn:relay1.expressturn.com:3480"],
-      username: "000000002080924083",
-      credential: "Qyviu5CZcybuLBOSmnzDO/NOsgY=",
+      urls: [
+        "turn:turn.dev.movmash.com:3478?transport=udp",
+        "turn:turn.dev.movmash.com:3478?transport=tcp",
+        "turns:turn.dev.movmash.com:5349?transport=tcp",
+      ],
+      username: "movmash",
+      credential: "movmash@123",
     },
   ],
 };
