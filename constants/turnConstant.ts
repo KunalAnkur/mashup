fdc122ea92b3bb43c2c434dd9cf41d5bf5a32f14
Ankur Kunal <ankur.kunal@ibm.com>
2025-12-13 14:29:04 +0530

    Update landing page

--- constants/turnConstant.ts
+++ constants/turnConstant.ts
@@ -4,7 +4,7 @@ const turnConfig = {
       urls: [
         "turn:turn.dev.movmash.com:3478?transport=udp",
         "turn:turn.dev.movmash.com:3478?transport=tcp",
-        "turns:turn.dev.movmash.com:5349?transport=tcp",
+        // "turns:turn.dev.movmash.com:5349?transport=tcp",
       ],
       username: "movmash",
       credential: "movmash@123",
